46459cc6e50d1dfd032f57b1b76db89a47200fb0
Believe20 <niveocerus@gmail.com>
2022-04-08 23:35:02 -0700

    created config code

--- config.py
+++ config.py
@@ -1,1 +1,28 @@
-TOKEN = 'OTYxNDY3NDQ4MjYyMDE3MDI0.Yk5aVg.zmIJBOYcHH6w0XDNkSSxx4KIcG0'
+import json
+
+TOKEN = ''
+
+try:
+	file = open('config.json', 'r')
+except FileNotFoundError:
+	print('Config file not created. Please run config.py.')
+	file = None
+
+if file != None:
+
+	configs = json.load(file)
+
+	if 'TOKEN' in configs:
+		TOKEN = configs['TOKEN']
+
+	file.close()
+
+if __name__ == '__main__':
+
+	key = input('Enter your discord bot token or press the Enter key to exit.\n> ')
+	if key != '':
+		configs = {
+			'TOKEN': key
+		}
+		with open('config.json', 'w') as file:
+			file.write(json.dumps(configs))
