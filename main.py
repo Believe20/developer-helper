3a203c7ca40212e165b5c473b622bbf1680e6902
Clawofdeath <blakeglover167@rocketmail.com>
2022-04-08 23:48:18 -0700

    finished python part

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from asyncio.windows_events import NULL
+from email.message import Message
 import discord
 import config
 
@@ -5,6 +7,8 @@ from urllib.request import urlopen
 python_docs_url = 'https://www.python.org/doc/versions/'
 
 client = discord.Client()
+Versionselect = False
+versions = []
 
 @client.event
 async def on_ready():
@@ -12,27 +16,39 @@ async def on_ready():
 
 @client.event
 async def on_message(message: discord.Message):
+	global Versionselect
+
 	if message.author == client.user:
 		return
 
 	if message.content.startswith('-hello'):
 		await message.channel.send('Hi.')
 	
-	await check_message(message)
+	if Versionselect == True:
+		await version_message(message)
+	else:
+		await check_message(message)
 
 async def check_message(message: discord.Message):
 	if message.content.startswith('-python'):
+		
+		global Versionselect		
+		global versions
 
+		Versionselect = True
 		def get_versions(html: str):
 			start_index = html.find('docs.python.org/release/')
-			end_index = html.find('docs.python.org/release/2.5.4/')
+			end_index = html.find('docs.python.org/release/1.4/')
 
-			versions = []
+			x = 0
 			for i in range(start_index, end_index):
 				if html[i] == 'P' and html[i:i+6] == 'Python':
-					phrase = html[i: i+20]
-					phrase_end = phrase.find('<')
-					versions.append(phrase[i:i+phrase_end])
+					phrase_end = html.find('<',i)
+					phrase = html[i:phrase_end]
+					#if phrase[phrase.find('.',phrase.find('.')+1)] == '.' and phrase[phrase.find('.',phrase.find('.')+1)+1] == '0' or len(phrase) < 11:
+					if phrase[phrase.find('.')+1] != x:
+						versions.append(html[i:phrase_end])
+						x = phrase[phrase.find('.')+1]
 
 			return versions
 
@@ -42,5 +58,29 @@ async def check_message(message: discord.Message):
 		versions = get_versions(html)
 		versions_string = '\n - '.join(versions)
 		await message.channel.send('Python selected! Which version do you need to know about?\n - ' + versions_string)
+
+	if message.content.startswith('-java SE'):
+
+		global Versionselect
+		global versions
+
+		
+
+
+async def version_message(message: discord.Message):
+	global Versionselect
+	global versions
+
+	mcon = message.content
+	if Versionselect == True:
+		for i in range(len(versions)): 
+			if message.content == versions[i]:
+				await message.channel.send('https://docs.python.org/release/' + mcon[mcon.find(' ') + 1 : len(mcon)] + '/\n')
+				Versionselect = False
+				versions = []
+				return
+
+
+
 
 client.run(config.TOKEN)
